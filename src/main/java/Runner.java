96986d9af17ad543ec2de97eea63393b64ffd20e
Aqil <100186541@lubbockisd.net>
2023-11-27 09:21:54 -0600

    Aqil Tijani

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,12 @@
 public class Runner {
     public static void main(String[] args){
+        System.out.println(StringySpells.middleFinder("aqil"));
+        System.out.println(StringySpells.alphaOmega("winner","dancer"));
+        System.out.println(StringySpells.subTract("aqil",3));
+        System.out.println(StringySpells.doFlip("tija","aqil"));
+        System.out.println(StringySpells.aveLen("tija","aqil"));
+
+
 
     }
 }
--- src/main/java/StringySpells.java
+++ src/main/java/StringySpells.java
@@ -2,6 +2,32 @@
 
 public class StringySpells {
 
+    public static String middleFinder(String text) {
+        return String.valueOf(text.charAt(text.length()/2));
+    }
+    public static String alphaOmega(String text1, String text2) {
+
+
+    return String.valueOf(text1.charAt(0))+text2.charAt(text2.length()-1);
+}
+
+public static String subTract(String text, int num){
+        return text.substring(0, num)+text.substring(num+1);
+}
+
+public static String doFlip(String text1, String text2){
+
+
+      return text2.charAt(0) + text1.substring(1);
+
+    }
+public static double aveLen(String text1, String text2) {
+        return (double) (text1.length() + text2.length()) /2;
+}
+
+
+
+
     // middleFinder
 
     // alphaOmega
