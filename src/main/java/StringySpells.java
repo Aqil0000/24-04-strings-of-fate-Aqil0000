7f6581f6c14d2f46600d671dde5bde9f6aab58c5
Aqil <100186541@lubbockisd.net>
2023-11-27 10:03:18 -0600

    Aqil Tijani

--- src/main/java/StringySpells.java
+++ src/main/java/StringySpells.java
@@ -2,7 +2,7 @@
 
 public class StringySpells {
 
-    public static String middleFinder(String text) {
+    public static String findMids(String text) {
         return String.valueOf(text.charAt(text.length()/2));
     }
     public static String alphaOmega(String text1, String text2) {
@@ -11,11 +11,11 @@ public class StringySpells {
     return String.valueOf(text1.charAt(0))+text2.charAt(text2.length()-1);
 }
 
-public static String subTract(String text, int num){
+public static String subtractive(String text, int num){
         return text.substring(0, num)+text.substring(num+1);
 }
 
-public static String doFlip(String text1, String text2){
+public static String flipper(String text1, String text2){
 
 
       return text2.charAt(0) + text1.substring(1);
